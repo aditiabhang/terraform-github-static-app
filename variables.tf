10305ec7e36519072b6b0a9d944509f2838a7e4d
Aditi Abhang <51350594+aditiabhang@users.noreply.github.com>
2024-11-20 18:46:12 +0000

    Updated destination_org

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "template_repo" {
 
 variable "destination_org" {
   description = "The name of the organization in Github that will contain the templated repo."
-  default     = "hashicorp-education"
+  default     = "aditiabhang"
 }
 
 variable "gh_token" {
